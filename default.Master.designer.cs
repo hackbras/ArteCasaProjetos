f403351e6cce9e1642e2dc0adaaeb960f1338406
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2018-10-31 10:32:42 -0300

    Front end responsivo

--- default.Master.designer.cs
+++ default.Master.designer.cs
@@ -22,6 +22,15 @@ namespace webFormsCodeista {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// pagetop control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl pagetop;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
